b03dc4ba78a8d3d8507a91f13b26ab4dda795852
Felipe Dutra <felipe.dutra@sydle.com>
2026-03-24 17:38:15 -0300

    fixed the imports in index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import 'dotenv/config';
-import PricesController from "./src/controllers/prices-controller.js";
-import MailService from './src/services/mailer.js';
-import SupportController from './src/controllers/support-controller.js';
+import PricesController from "./controllers/prices-controller.js";
+import MailService from './services/mailer.js';
+import SupportController from './controllers/support-controller.js';
 
 const pricesController = new PricesController();
 const mailService = new MailService();
